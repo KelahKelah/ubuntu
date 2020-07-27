e228bb290044ddacde6dc40cd36f84a3a52796cc
KelahKelah <kelechiekebor@gmail.com>
2020-07-27 06:18:36 +0100

    Modified icons

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
-    $('i').hover(function() {
-        $('.fa').css("color", "#fff");
+
+    $('i').hover(high, high) 
+        $('.fa, #date').css("color", "#fff");
         // event.preventDefault();
-        
-    })
+    
+    function high() {
+        $('i').toggleClass('high')
+    }
+
+    $('body').click(function() {
+        $('.wrappy').hide()
+    }
+    )
     // $("div p i").click(function() {
     //     $("p i").removeClass("active")
     //     $("this").addClass("active")
@@ -20,39 +28,7 @@ $(document).ready(function(){
         $('li').removeClass("active");
         $(this).addClass("active");
     });
-    // $("i").css("border", "red solid 2px");
-    // $('p').css("color","blue");
-    // $('p').click(function() {
-    //     $('div > dt > p').css('background-color', '#ff8030');
-    // })
-    
-    // $("button").click(function() {
-    //     $("p").show();
-    // })
-    // $("h4").hover(function() {
-    //     $("h4").hide();
-    // })
-    // $("#myId").dblclick(function() {
-    //     alert("nice one") 
-    // })
-    // $(".myClass").dblclick(function() {
-    //     $("p").css("color", "blue")
-    // })
-    // $(".first").dblclick(function() {
-    //     $("second").hide();
-    // })
-    // $('i').hover(function() {
-    //     $('div').toggle(3000);
-    // })
-    // $('h2').hover(function() {
-    //     $('.our-services-item').fadeIn(1000);
-    //     $('.our-services-item').fadeIn(2000);
-    //     $('.our-services-item').fadeIn(1000);
-    //     $('.our-services-item').fadeIn(2000);
-    // })
-    
-    // $("<p>").css("border", "solid 2px red");
-
+     
 });
 
 
